95b375dea989eb413e73a833d79bea06c5d37934
Maakis <tmaakis@gmail.com>
2025-12-15 14:55:09 +0000

    initial commit

---
+++ src/main/java/com/utilitygang/zerosum/ZerosumApplication.java
@@ -0,0 +1,13 @@
+package com.utilitygang.zerosum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerosumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerosumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utilitygang/zerosum/ZerosumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utilitygang.zerosum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerosumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
