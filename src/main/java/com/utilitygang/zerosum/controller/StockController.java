53f64ffb884c92d04eba338a5df291eaeaca25f9
Maakis <tmaakis@gmail.com>
2025-12-16 15:41:54 +0000

    remove the 3 non mvp methods

--- src/main/java/com/utilitygang/zerosum/controller/StockController.java
+++ src/main/java/com/utilitygang/zerosum/controller/StockController.java
@@ -11,26 +11,6 @@ import org.springframework.ui.Model;
 
 @Controller
 public class StockController {
-    @GetMapping("/stocks")
-    public String stocksIndex(@AuthenticationPrincipal OidcUser principal, Model model) {
-        return "stocks/index";
-    }
-
-    // pass the type of large (for cards in /stocks) or small (for cards in /index)
-    // default to large cards
-    @GetMapping("/stocks/list/{type}")
-    public String stocksList(@PathVariable String type, @AuthenticationPrincipal OidcUser principal, Model model) {
-        if (type.equals("small"))
-            return "stocks/fragment/bigcards";
-        else
-            return "stocks/fragment/smallcards";
-    }
-
-    @GetMapping("/stocks/{id}")
-    public String stocksIndividual(@PathVariable Long id, @AuthenticationPrincipal OidcUser principal, Model model) {
-        return "stocks/individual";
-    }
-
     @PostMapping("/stocks/{id}/buy")
     public RedirectView stocksBuy(@PathVariable Long id, @AuthenticationPrincipal OidcUser principal, Model model) {
         return new RedirectView("/stocks" + id);
