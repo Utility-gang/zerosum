cdd026e328bba3fcac08da9295a49560b87fd0de
tashi-ibidapo <tashi.ibidapo@gmail.com>
2025-12-16 15:48:35 +0000

    Started Project Architecture

---
+++ src/main/java/com/utilitygang/zerosum/Controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.utilitygang.zerosum.Controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "ok";
+    }
+}
