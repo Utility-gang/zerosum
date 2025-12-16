53c43e25ac29c28295706c30b8a8f53d65da6306
ShakedKirsh <128934937+ShakedKirsh@users.noreply.github.com>
2025-12-16 15:59:33 +0000

    merged migrations and corrected data types

--- src/main/resources/db.migration/V1_create_users_table.sql
+++ src/main/resources/db.migration/V1_create_users_table.sql
@@ -1,7 +1,16 @@
-DROP TABLE IF EXISTS users;
-
 CREATE TABLE users (
     id bigserial PRIMARY KEY,
     username varchar(50) NOT NULL UNIQUE,
-    cash DOUBLE
+    cash Decimal(19,4)
+);
+
+CREATE TABLE stocks (
+    id bigserial PRIMARY KEY,
+    symbol TEXT NOT NULL UNIQUE,
+    logo TEXT,
+    last_change TIMESTAMP,
+    amount INTEGER,
+    owner_id BIGINT NOT NULL,
+    CONSTRAINT fk_user_stocks FOREIGN KEY (owner_id)
+         REFERENCES users(id)
 );
