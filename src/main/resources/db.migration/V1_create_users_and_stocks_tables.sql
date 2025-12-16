cc2e0e869e586d496add78fb94d417f51c3016f7
ShakedKirsh <128934937+ShakedKirsh@users.noreply.github.com>
2025-12-16 16:32:59 +0000

    created companies table

--- src/main/resources/db.migration/V1_create_users_and_stocks_tables.sql
+++ src/main/resources/db.migration/V1_create_users_and_stocks_tables.sql
@@ -6,11 +6,17 @@ CREATE TABLE users (
 
 CREATE TABLE stocks (
     id bigserial PRIMARY KEY,
-    symbol TEXT NOT NULL,
-    logo TEXT,
-    last_change TIMESTAMP,
     amount INTEGER,
     owner_id BIGINT NOT NULL,
+    company_id BIGINT NOT NULL,
     CONSTRAINT fk_user_stocks FOREIGN KEY (owner_id)
          REFERENCES users(id)
+    CONSTRAINT fk_company_stocks FOREIGN KEY (company_id)
+         REFERENCES companies(id)
+);
+
+CREATE TABLE companies (
+    id bigserial PRIMARY KEY,
+    symbol TEXT NOT NULL UNIQUE,
+    logo TEXT
 );
