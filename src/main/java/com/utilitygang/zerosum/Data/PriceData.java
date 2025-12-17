b2fa264a6b9e5ecb4168ccd3c813aa4a97f5f53f
Sam llewellyn <167631312+s111ew@users.noreply.github.com>
2025-12-17 12:40:55 +0000

    feat: use ConcurrentHashMap instead of HashMap for storing price data in a frequent read/write environment

--- src/main/java/com/utilitygang/zerosum/Data/PriceData.java
+++ src/main/java/com/utilitygang/zerosum/Data/PriceData.java
@@ -1,17 +1,25 @@
 package com.utilitygang.zerosum.Data;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class PriceData {
-    private static final Map<String, Double> prices = new HashMap<>();
+    // using a 'ConcurrentHashMap' data structure here instead of a 'HashMap'
+    // as its thread safe when doing lots of concurrent reads/writes, imagine
+    // it utilises some sort of lock
+    // https://www.baeldung.com/java-concurrent-map
+    private static final Map<String, Double> prices = new ConcurrentHashMap<>();
 
-    public static void setPrice(String ticker, Double price) {
-        if (ticker != null && price != null) {
-            prices.put(ticker, price);
+    public static void setPrice(String symbol, Double price) {
+        if (symbol != null && price != null) {
+            prices.put(symbol, price);
         }
     }
 
-    public static Double getPrice(String ticker) {
-        return prices.get(ticker);
+    // 'getOrDefault' method is recommended instead of 'get' for 'ConcurrentHashMap'
+    // because you always have a value to read even if a write has removed the value
+    // a millisecond before you try to access it
+    public static Double getPrice(String symbol) {
+        return prices.getOrDefault(symbol, 0.0);
     }
 }
