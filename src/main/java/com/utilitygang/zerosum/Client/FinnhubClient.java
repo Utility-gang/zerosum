e66c421c6845161d1f50de03760d37c7d79dafef
Sam llewellyn <167631312+s111ew@users.noreply.github.com>
2025-12-17 13:07:00 +0000

    fix: ignore ping responses

--- src/main/java/com/utilitygang/zerosum/Client/FinnhubClient.java
+++ src/main/java/com/utilitygang/zerosum/Client/FinnhubClient.java
@@ -2,6 +2,7 @@ package com.utilitygang.zerosum.Client;
 
 import java.net.URI;
 import java.nio.ByteBuffer;
+import java.util.Objects;
 
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.drafts.Draft;
@@ -32,9 +33,26 @@ public class FinnhubClient extends WebSocketClient {
 
     @Override
     public void onMessage(String response) {
+        // get the head of the JSON object
         JSONObject root = new JSONObject(response);
+
+        // check the 'type' property as sometimes it just sends
+        // "type":"ping" so ignore those
+        String type = root.getString("type");
+        if (!Objects.equals(type, "trade")) {
+            return;
+        }
+
+        // if it is "type":"data" we get the data array
+        // which contains all the trade info
         JSONArray data = root.getJSONArray("data");
+
+        // get the trade at index 0 because sometimes it sends
+        // multiple but we hope (?) that the price doesnt change
+        // in that time
         JSONObject trade = data.getJSONObject(0);
+
+        // extract the symbol (ticker) and the price from the trade info
         String symbol = trade.getString("s");
         Double price = trade.getDouble("p");
 
