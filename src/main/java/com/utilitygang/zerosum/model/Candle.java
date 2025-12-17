119a54a0cf750663e7a7103e48ad23442b8bff4e
tashi-ibidapo <tashi.ibidapo@gmail.com>
2025-12-17 12:07:26 +0000

    Candle model complete

---
+++ src/main/java/com/utilitygang/zerosum/model/Candle.java
@@ -0,0 +1,10 @@
+package com.utilitygang.zerosum.model;
+
+public class Candle {
+    private long timestamp;
+    private double open;
+    private double high;
+    private double low;
+    private double close;
+    private long volume;
+}
