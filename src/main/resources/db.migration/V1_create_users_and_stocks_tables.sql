5fa54fb0cfdce6ff2f76a052e9a3dacd52f2fefe
Anna Veselova <anna1995correspondence@gmail.com>
2025-12-17 11:39:39 +0000

    fix: fixes migration V1

--- src/main/resources/db.migration/V1_create_users_and_stocks_tables.sql
+++ src/main/resources/db.migration/V1_create_users_and_stocks_tables.sql
@@ -4,13 +4,18 @@ CREATE TABLE users (
     cash Decimal(19,2)
 );
 
+CREATE TABLE companies (
+    symbol TEXT PRIMARY KEY,
+    logo TEXT
+);
+
 CREATE TABLE stocks (
     id bigserial PRIMARY KEY,
     amount DOUBLE PRECISION,
     owner_id BIGINT NOT NULL,
     company_id TEXT NOT NULL,
     CONSTRAINT fk_user_stocks FOREIGN KEY (owner_id)
-         REFERENCES users(id)
+         REFERENCES users(id),
     CONSTRAINT fk_company_stocks FOREIGN KEY (company_id)
          REFERENCES companies(symbol)
 );
