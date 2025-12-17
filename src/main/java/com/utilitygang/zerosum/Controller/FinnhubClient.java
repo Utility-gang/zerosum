895fa54148d91f2a8ff6fe0a1888cb309d679df2
ShakedKirsh <128934937+ShakedKirsh@users.noreply.github.com>
2025-12-17 11:33:31 +0000

    opened websocket connection

---
+++ src/main/java/com/utilitygang/zerosum/Controller/FinnhubClient.java
@@ -0,0 +1,46 @@
+package com.utilitygang.zerosum.Controller;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.handshake.ServerHandshake;
+
+public class FinnhubClient extends WebSocketClient {
+
+    public FinnhubClient(URI serverUri, Draft draft) {
+        super(serverUri, draft);
+    }
+
+    public FinnhubClient(URI serverURI) {
+        super(serverURI);
+    }
+
+    @Override
+    public void onOpen(ServerHandshake handshakedata) {
+        send("{\"type\":\"subscribe\",\"symbol\":\"BINANCE:BTCUSDT\"}");
+        System.out.println("new websocket connection opened");
+    }
+
+    @Override
+    public void onClose(int code, String reason, boolean remote) {
+        System.out.println("closed with exit code " + code + " additional info: " + reason);
+    }
+
+    @Override
+    public void onMessage(String message) {
+        System.out.println("received message: " + message);
+    }
+
+    @Override
+    public void onMessage(ByteBuffer message) {
+        System.out.println("received ByteBuffer");
+    }
+
+    @Override
+    public void onError(Exception ex) {
+        System.err.println("an error occurred:" + ex);
+    }
+}
