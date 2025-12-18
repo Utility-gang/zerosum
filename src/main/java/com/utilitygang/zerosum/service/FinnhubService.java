9a2f8a45a936a60ecb8ce9ece0a12c34cdd8ba07
Sam llewellyn <167631312+s111ew@users.noreply.github.com>
2025-12-18 11:21:11 +0000

    feat: add dotenv dependency and move Finnhub API Key to .env file

--- src/main/java/com/utilitygang/zerosum/service/FinnhubService.java
+++ src/main/java/com/utilitygang/zerosum/service/FinnhubService.java
@@ -2,6 +2,7 @@ package com.utilitygang.zerosum.service;
 
 import com.utilitygang.zerosum.client.FinnhubClient;
 import com.utilitygang.zerosum.repository.CompanyRepository;
+import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,11 @@ public class FinnhubService {
 
     @PostConstruct
     public void start() throws Exception {
-        URI uri = new URI("wss://ws.finnhub.io?token=d518fghr01qjia5c0t00d518fghr01qjia5c0t0g");
+        Dotenv dotenv = Dotenv.load();
+        String finnhubKey = dotenv.get("FINNHUB_API_KEY");
+
+        String url = String.format("wss://ws.finnhub.io?token=%s", finnhubKey);
+        URI uri = new URI(url);
         FinnhubClient client = new FinnhubClient(uri, companyRepository);
         client.connect();
     }
