46e86ff6f64377ed590b0b7bda3a46fce9d8ec5d
Maakis <tmaakis@gmail.com>
2025-12-18 16:57:54 +0000

    initial portfolio controller

--- src/main/java/com/utilitygang/zerosum/controller/PortfolioController.java
+++ src/main/java/com/utilitygang/zerosum/controller/PortfolioController.java
@@ -1,13 +1,32 @@
 package com.utilitygang.zerosum.controller;
 
+import com.utilitygang.zerosum.model.*;
+import com.utilitygang.zerosum.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class PortfolioController {
+    @Autowired
+    CompanyRepository companyRepo;
+    @Autowired
+    UserRepository userRepo;
+    @Autowired
+    StockRepository stockRepo;
+
     @GetMapping("/portfolio")
-    public String portfolioIndex(Model model) {
-        return "portfolio/index";
+    public String portfolioIndex(@AuthenticationPrincipal DefaultOidcUser principal, Model model) {
+        User owner = userRepo.findUserByUsername((String) principal.getAttribute("email")).get();
+        List<Company> companies = companyRepo.findAllByStockOwner(owner);
+
+        model.addAttribute("companies", companies);
+
+        return ("/index");
     }
 }
