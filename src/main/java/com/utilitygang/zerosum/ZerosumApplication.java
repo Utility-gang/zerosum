664857e5bc7ae8bed865db3bd4b7859cf985c618
Sam llewellyn <167631312+s111ew@users.noreply.github.com>
2025-12-18 09:57:43 +0000

    feat: add @PostConstruct decorator to FinnhubService to run the start() function on Application start

--- src/main/java/com/utilitygang/zerosum/ZerosumApplication.java
+++ src/main/java/com/utilitygang/zerosum/ZerosumApplication.java
@@ -1,17 +1,12 @@
 package com.utilitygang.zerosum;
 
-import com.utilitygang.zerosum.service.FinnhubService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ZerosumApplication {
 
-	public static void main(String[] args) throws Exception {
-        ConfigurableApplicationContext context = SpringApplication.run(ZerosumApplication.class, args);
-
-        FinnhubService finnhubService = context.getBean(FinnhubService.class);
-        finnhubService.start();
+	public static void main(String[] args) {
+        SpringApplication.run(ZerosumApplication.class, args);
 	}
 }
--- src/main/java/com/utilitygang/zerosum/service/FinnhubService.java
+++ src/main/java/com/utilitygang/zerosum/service/FinnhubService.java
@@ -2,6 +2,7 @@ package com.utilitygang.zerosum.service;
 
 import com.utilitygang.zerosum.client.FinnhubClient;
 import com.utilitygang.zerosum.repository.CompanyRepository;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
 import java.net.URI;
@@ -15,6 +16,7 @@ public class FinnhubService {
         this.companyRepository = companyRepository;
     }
 
+    @PostConstruct
     public void start() throws Exception {
         URI uri = new URI("wss://ws.finnhub.io?token=d518fghr01qjia5c0t00d518fghr01qjia5c0t0g");
         FinnhubClient client = new FinnhubClient(uri, companyRepository);
