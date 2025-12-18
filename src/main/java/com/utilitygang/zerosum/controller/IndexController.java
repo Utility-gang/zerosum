a743877da0d892ca7216f0a4edec942ed69b7c15
Maakis <tmaakis@gmail.com>
2025-12-18 16:59:49 +0000

    fix auth0

--- src/main/java/com/utilitygang/zerosum/controller/IndexController.java
+++ src/main/java/com/utilitygang/zerosum/controller/IndexController.java
@@ -5,8 +5,7 @@ import com.utilitygang.zerosum.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
@@ -16,7 +15,7 @@ public class IndexController {
     @Autowired
     CompanyRepository companyRepository;
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index(Model model) {
         List<Company> companies = companyRepository.findAll();
 
