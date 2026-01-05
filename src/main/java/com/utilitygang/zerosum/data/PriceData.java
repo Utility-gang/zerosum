5ac3adebb209bff33942780b34561c73c4e8f023
Maakis <tmaakis@gmail.com>
2026-01-05 13:43:33 +0000

    add serialisation

--- src/main/java/com/utilitygang/zerosum/data/PriceData.java
+++ src/main/java/com/utilitygang/zerosum/data/PriceData.java
@@ -1,15 +1,45 @@
 package com.utilitygang.zerosum.data;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.math.BigDecimal;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.annotation.PreDestroy;
 
 public class PriceData {
     // using a 'ConcurrentHashMap' data structure here instead of a 'HashMap'
     // as its thread safe when doing lots of concurrent reads/writes, imagine
     // it utilises some sort of lock
     // https://www.baeldung.com/java-concurrent-map
-    private static final Map<String, BigDecimal> prices = new ConcurrentHashMap<>();
+    private static Map<String, BigDecimal> prices = new ConcurrentHashMap<>();
+
+    public static void deserialise() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            // this really wants me to handle the exceptions
+            prices = mapper.readValue(Paths.get("prices.json").toFile(), new TypeReference<Map<String, BigDecimal>>() {
+            });
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    @PreDestroy
+    public static void serialise() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            // this really wants me to handle the exceptions
+            mapper.writeValue(Paths.get("prices.json").toFile(), new ConcurrentHashMap<String, BigDecimal>(prices));
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
 
     public static void setPrice(String symbol, BigDecimal price) {
         if (symbol != null && price != null) {
--- src/main/java/com/utilitygang/zerosum/service/FinnhubService.java
+++ src/main/java/com/utilitygang/zerosum/service/FinnhubService.java
@@ -1,6 +1,7 @@
 package com.utilitygang.zerosum.service;
 
 import com.utilitygang.zerosum.client.FinnhubClient;
+import com.utilitygang.zerosum.data.PriceData;
 import com.utilitygang.zerosum.model.Company;
 import com.utilitygang.zerosum.repository.CompanyRepository;
 import io.github.cdimascio.dotenv.Dotenv;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.File;
 import java.math.BigDecimal;
 import java.net.URI;
 import java.util.Map;
@@ -33,21 +35,27 @@ public class FinnhubService {
         client.connect();
     }
 
-    // when the app starts up, send GET requests to the quote endpoint
-    // and update the cached price
+    // when the app starts up, deserialise PriceData.price
+    // or, if there's no file, send GET requests to the quote endpoint and update
+    // the cached price
     @PostConstruct
     public void updateCachedPrices() throws Exception {
-        RestTemplate restTemplate = new RestTemplate();
+        File f = new File("prices.json");
+        if (f.exists()) {
+            PriceData.deserialise();
+        } else {
+            RestTemplate restTemplate = new RestTemplate();
 
-        for (Company company : companyRepository.findAll()) {
-            String url = String.format("https://finnhub.io/api/v1/quote?symbol=%s&token=%s", company.getSymbol(),
-                    finnhubKey);
+            for (Company company : companyRepository.findAll()) {
+                String url = String.format("https://finnhub.io/api/v1/quote?symbol=%s&token=%s", company.getSymbol(),
+                        finnhubKey);
 
-            Map<String, Object> response = restTemplate.getForObject(url, Map.class);
-            if (response != null && response.containsKey("c")) {
-                Double currentPrice = Double.valueOf(response.get("c").toString());
-                company.setCachedPrice(BigDecimal.valueOf(currentPrice));
-                companyRepository.save(company);
+                Map<String, Object> response = restTemplate.getForObject(url, Map.class);
+                if (response != null && response.containsKey("c")) {
+                    Double currentPrice = Double.valueOf(response.get("c").toString());
+                    company.setCachedPrice(BigDecimal.valueOf(currentPrice));
+                    companyRepository.save(company);
+                }
             }
         }
     }
