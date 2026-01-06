45e768e8861133d3845687010b5845ccb5975d96
Sam llewellyn <167631312+s111ew@users.noreply.github.com>
2026-01-06 15:12:55 +0000

    feat: add methods to portfolio service to keep cached portfolio values updated every 5 minutes

--- src/main/java/com/utilitygang/zerosum/service/PortfolioService.java
+++ src/main/java/com/utilitygang/zerosum/service/PortfolioService.java
@@ -4,7 +4,9 @@ import com.utilitygang.zerosum.data.PriceData;
 import com.utilitygang.zerosum.model.Stock;
 import com.utilitygang.zerosum.model.User;
 import com.utilitygang.zerosum.repository.StockRepository;
+import com.utilitygang.zerosum.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import static com.utilitygang.zerosum.data.PriceData.getPriceForStockAmount;
@@ -14,9 +16,14 @@ import java.util.List;
 
 @Service
 public class PortfolioService {
+    // update the cached portfolio values every 5 minutes
+    private static final long REFRESH_RATE = 10 * 60 * 500;
 
     @Autowired
     private StockRepository stockRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     /**
      * Calculates just the total value of stocks (excluding cash)
@@ -34,5 +41,24 @@ public class PortfolioService {
     public BigDecimal calculateTotalPortfolioValue(User user) {
         // Total portfolio = cash + stock values
         return user.getCash().add(calculateTotalStockValue(user));
+    }
+
+    // update a single user's protfolio value
+    public void updateCachedPortfolioValue(User user) {
+        BigDecimal newPortfolioValue = calculateTotalPortfolioValue(user);
+        user.setPortfolioValue(newPortfolioValue);
+        userRepository.save(user);
+    }
+
+    // every 5 minutes, update the cached ptf values for all users
+    @Scheduled(fixedRate = REFRESH_RATE)
+    public void updateAllCachedPortfolioValues() {
+        // get all users
+        List<User> allUsers = userRepository.findAll();
+
+        // update the cached ptf value for each one
+        for (User user : allUsers) {
+            updateCachedPortfolioValue(user);
+        }
     }
 }
