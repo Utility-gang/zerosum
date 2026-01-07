846cbf3dbb49f046552726ba752e252f0285fb53
ShakedKirsh <128934937+ShakedKirsh@users.noreply.github.com>
2026-01-07 14:09:22 +0000

    buy/sell legends, fixed logic

--- src/main/java/com/utilitygang/zerosum/controller/StockController.java
+++ src/main/java/com/utilitygang/zerosum/controller/StockController.java
@@ -37,12 +37,14 @@ public class StockController {
             attr.addFlashAttribute("msg", "You do not have enough money.");
         } else {
             Stock stock;
-            if (stockRepo.findByOwnerAndCompanySymbol(owner, company_id).isEmpty())
+            if (stockRepo.findByOwnerAndCompanySymbol(owner, company_id).isEmpty()) {
                 stock = new Stock(amount, owner, companyRepo.findById(company_id).get());
-            else
+            } else {
                 stock = stockRepo.findByOwnerAndCompanySymbol(owner, company_id).get();
+            }
             stockRepo.save(stock);
             owner.setCash(owner.getCash().subtract(stock_value));
+            userRepo.save(owner);
         }
         return "redirect:" + req.getHeader("Referer");
     }
@@ -63,8 +65,13 @@ public class StockController {
                 attr.addFlashAttribute("msg", "You do not hold this much " + company_id + " stock.");
             } else {
                 owner.setCash(stock_value.add(owner.getCash()));
-                stock.setAmount(stock.getAmount() - amount);
-                stockRepo.save(stock);
+                userRepo.save(owner);
+                if (stock.getAmount() - amount == 0) {
+                    stockRepo.delete(stock);
+                } else {
+                    stock.setAmount(stock.getAmount() - amount);
+                    stockRepo.save(stock);
+                }
             }
         }
         return "redirect:" + req.getHeader("Referer");
