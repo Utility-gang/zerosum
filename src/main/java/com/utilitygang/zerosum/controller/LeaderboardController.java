2455aa0b5f6adc1d520fd0c31ad4fbbed46aa69a
Sam llewellyn <167631312+s111ew@users.noreply.github.com>
2026-01-07 13:30:16 +0000

    feat: add isAuthenticated variable to news & leaderboard models, to display correct login/logout button in navbar

--- src/main/java/com/utilitygang/zerosum/controller/LeaderboardController.java
+++ src/main/java/com/utilitygang/zerosum/controller/LeaderboardController.java
@@ -2,7 +2,10 @@ package com.utilitygang.zerosum.controller;
 
 import com.utilitygang.zerosum.model.User;
 import com.utilitygang.zerosum.repository.UserRepository;
+import com.utilitygang.zerosum.service.UserContextService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +18,26 @@ public class LeaderboardController {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    UserContextService userContextService;
+
     @GetMapping({ "/leaderboard" })
-    public String leadboard(Model model) {
+    public String leadboard(Model model, @AuthenticationPrincipal DefaultOidcUser principal) {
         List<User> topUsers = userRepository.findTop10ByOrderByPortfolioValueAsc();
 
         model.addAttribute("topUsers", topUsers);
+
+        boolean isAuthenticated = principal != null;
+        model.addAttribute("isAuthenticated", isAuthenticated);
+        // Add portfolio value for navbar
+        if (isAuthenticated) {
+            User user = userContextService.getUser(principal);
+
+            if (user != null) {
+                model.addAttribute("totalPortfolioValue", userContextService.getUserPortfolioValue(user));
+                model.addAttribute("user", user);
+            }
+        }
 
         return "leaderboard";
     }
